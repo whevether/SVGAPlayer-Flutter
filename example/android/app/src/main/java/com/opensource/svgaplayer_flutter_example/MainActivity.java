d1701ec0711cd132efe6a44a00bb1490298b18d5
keep.wan <whevether@outlook.com>
2024-10-23 15:21:50 +0800

    update

--- example/android/app/src/main/java/com/opensource/svgaplayer_flutter_example/MainActivity.java
+++
@@ -1,8 +0,0 @@
-package com.opensource.svgaplayer_flutter_example;
-
-import android.os.Bundle;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
